078f3e744cfd7102145acb4a4d12957d89d8bbf2
Arthelon <hsing.daniel@gmail.com>
2016-03-03 21:23:59 +0800

    Added Submission and Room db models

--- models.js
+++ models.js
@@ -1,14 +1,35 @@
 var mongoose = require('mongoose')
+var Schema = mongoose.Schema
 var findOrCreate = require('mongoose-findorcreate')
 
 var models = {}
 
-var UserSchema = new mongoose.Schema({
+var SubmissionSchema = new Schema({
+    timestamp: {type: Date, default: Date.now},
+    name: String,
+    desc: String,
+    user: {type: String, require: true},
+    file: {type: Buffer, required: true}
+})
+
+var RoomSchema = new Schema({
+    _id: {type: String, required:true, unique: true},
+    name: {type: String, required:true},
+    desc: String,
+    submissions: [
+        {type: Schema.Types.ObjectId, ref: 'Submission'}
+    ]
+})
+
+var UserSchema = new Schema({
     username: {type:String, unique: true, required: true},
     password: {type:String, required: true},
     first_name: {type:String, required: true},
     last_name: {type:String, required: true},
-    email: {type:String, unique: true, required: true}
+    email: {type:String, unique: true, required: true},
+    rooms: [
+        {type: Schema.Types.ObjectId, ref: 'Room'}
+    ]
 })
 UserSchema.methods.verifyPassword = function (password) {
     "use strict";
@@ -17,8 +38,11 @@ UserSchema.methods.verifyPassword = function (password) {
 }
 UserSchema.methods.validatePassword = function() {
     return !!this.password.match(/^[\d\w]{8,}$/);
-
 }
 UserSchema.plugin(findOrCreate)
+
+models.Submission = mongoose.model('Submission', SubmissionSchema)
+models.Room = mongoose.model('Room', RoomSchema)
 models.User = mongoose.model('User', UserSchema)
+
 module.exports = models
