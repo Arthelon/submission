8b4640204cafd6b9a3ef604b2100091424f35b26
Arthelon <hsing.daniel@gmail.com>
2016-03-13 21:29:38 +0800

    Added room validation middleware, added new API resource for Problems

--- routes/room.js
+++ routes/room.js
@@ -28,6 +28,7 @@ var upload = multer({
 var Room = models.Room
 var File = models.File
 var Submission = models.Submission
+var Problem = models.Problem
 
 router.route('/:room_name')
     .get(function(req, res) {
@@ -111,54 +112,84 @@ router.route('/:room_name')
         })
     })
 
-router.get('/_download/:room_name/:submission', function(req, res) {
-    if (req.user) {
-        var room_name = req.params.room_name
-        Room.findOne({
-            name: room_name
-        }, function(err, room) {
-            if (err) throw err
-            if (room.owner.toString() != req.user._id.toString()) {
-                res.end(JSON.stringify({
-                    status: 'FAILED',
-                    msg: 'User does not own room'
-                }))
-            } else {
-                Submission
-                    .findOne({
-                        name: req.params.submission
-                    })
-                    .populate('files')
-                    .exec(function(err, sub) {
+router.get('/_download/:room_name/:submission', validateRoom,
+    function(req, res) {
+        Submission
+            .findOne({
+                name: req.params.submission
+            })
+            .populate('files')
+            .exec(function(err, sub) {
+                if (err) throw err
+                if (sub.files.length == 1) {
+                    res.download('uploads/'+sub.files[0].loc, sub.files[0].name, (err) => {
                         if (err) throw err
-                        if (sub.files.length == 1) {
-                            res.download('uploads/'+sub.files[0].loc, sub.files[0].name, (err) => {
-                                if (err) throw err
-                            })
-                        } else {
-                            var file_bundle = archiver.create('zip', {})
-                            file_bundle.pipe(res)
-                            sub.files.forEach(function (file) {
-                                file_bundle.append(fs.createReadStream('uploads/'+file.loc), {
-                                    name: file.name
-                                })
-                            })
-                            file_bundle.finalize()
-                        }
                     })
-            }
+                } else {
+                    var file_bundle = archiver.create('zip', {})
+                    file_bundle.pipe(res)
+                    sub.files.forEach(function (file) {
+                        file_bundle.append(fs.createReadStream('uploads/'+file.loc), {
+                            name: file.name
+                        })
+                    })
+                    file_bundle.finalize()
+                }
+            })
+    }
+)
+
+router.delete('/:room_name/_remove_sub/:submission', validateRoom,
+    function(req, res) {
+        var room = req.room
+        Submission.findOneAndRemove({name: req.params.submission}, function(err, sub) {
+            if (err) throw err
+            Room.findOneAndUpdate({name: req.params.room_name},  {
+                $pull: {
+                    submissions: sub._id
+                }
+            },function(err) {
+                if (err) throw err
+            })
+            sub.files.forEach(function(id) {
+                File.findOneAndRemove({_id: id}, (err, file) => {
+                    if (err) throw err
+                    fs.unlink('uploads/'+file.loc, (err) => {
+                        if (err) throw err
+                    })
+                })
+            })
+         })
+    })
+
+router.get('/:room_name/_problems', validateRoom, function(req, res) {
+    var room = req.room
+    Problem.find({}, function(err, problems) {
+        if (err) throw err
+        var payload = {
+            status: 'OK',
+            problems: [
+
+            ]
+        }
+        problems.forEach(function(problem) {
+            payload.problems.push({
+                name: problem.name,
+                desc: problem.desc
+            })
         })
-    } else {
-        res.status(404)
+        res.end(JSON.stringify(payload))
+    })
+})
+
+function validateRoom(req, res, next) {
+    if (!req.user) {
+        res.status(400)
         res.end(JSON.stringify({
             status: 'FAILED',
             msg: 'Unvalidated user'
         }))
-    }
-})
-
-router.delete('/:room_name/_remove_sub/:submission', function(req, res) {
-    if (req.user) {
+    } else {
         Room.findOne({name:req.params.room_name}, function(err, room) {
             if (err) throw err
             if (req.user._id.toString() != room.owner.toString()) {
@@ -167,45 +198,11 @@ router.delete('/:room_name/_remove_sub/:submission', function(req, res) {
                     msg: 'User does not own room'
                 }))
             } else {
-                Submission.findOneAndRemove({name: req.params.submission}, function(err, sub) {
-                    if (err) throw err
-                    Room.findOneAndUpdate({name: req.params.room_name},  {
-                        $pull: {
-                            submissions: sub._id
-                        }
-                    },function(err) {
-                        if (err) throw err
-                    })
-                    sub.files.forEach(function(id) {
-                        File.findOneAndRemove({_id: id}, (err, file) => {
-                            if (err) throw err
-                            fs.unlink('uploads/'+file.loc, (err) => {
-                                if (err) throw err
-                            })
-                        })
-                    })
-                 })
+                req.room = room
+                next()
             }
         })
-    } else {
-        res.status(404)
-        res.end(JSON.stringify({
-            status: 'FAILED',
-            msg: 'Unvalidated user'
-        }))
     }
-})
-
-//router.get('/:room_name/_problems', function(req, res) {
-//    if (!req.user) {
-//        res.status(400)
-//        res.end(JSON.stringify({
-//            status: 'FAILED',
-//            msg: 'Unvalidated user'
-//        }))
-//    } else {
-//        Room.fin
-//    }
-//})
+}
 
 module.exports = router
