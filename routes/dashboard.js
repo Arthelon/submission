d47b2909a94d6f178d4d9304469fdb38ce2ff2a3
Arthelon <hsing.daniel@gmail.com>
2016-04-06 17:53:02 +0800

    Removed unnecessary database lookup from dashboard root route

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,21 +3,12 @@ var router = express.Router()
 var models = require('../models')
 
 router.route('/')
-    .get(function (req, res, next) {
+    .get(function (req, res) {
         if (req.user) {
-            models.User
-                .findOne({
-                    username: req.user.username
-                })
-                .populate('rooms')
-                .sort('username')
-                .exec(function (err, user) {
-                    res.render('dashboard', {
-                        name: user.username,
-                        title: 'submission | Dashboard',
-                        rooms: user.rooms
-                    })
-                })
+            res.render('dashboard', {
+                name: req.user.username,
+                title: 'submission | Dashboard'
+            })
         } else {
             res.redirect('/')
         }
