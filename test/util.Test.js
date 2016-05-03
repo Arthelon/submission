3ca36635fb984e07868a5f60dc376d12c11ddc7f
Arthelon <hsing.daniel@gmail.com>
2016-05-03 10:07:46 +0800

    added more tests for the util modulee

--- test/util.Test.js
+++ test/util.Test.js
@@ -7,9 +7,11 @@ var mongoose = require('mongoose')
 var models = require('../models')
 
 var Room = models.Room
+var User = models.User
 
 var handleResp = util.handleResp
 var validateRoom = util.validateRoom
+var validateBody = util.validateBody
 
 
 describe('util', function() {
@@ -18,11 +20,7 @@ describe('util', function() {
             if (err) throw err
         })
     })
-        
-    
-    // after(function() {
-    //     mongoose.connection.close()
-    // })
+
     describe('#handleResp()', function() {
         var res = {
             json: sinon.spy(),
@@ -38,17 +36,40 @@ describe('util', function() {
         })
     })
     describe('#validateRoom()', function() {
+        var room_user;
+        //Hooks
         before(function() {
-            Room.create({
-                name: 'TestRoom',
-                desc: 'room for tests'
+            User.create({
+                username: 'test_user',
+                email: 'test@randomdomain.com',
+                password: '123456789',
+                first_name: 'test_first',
+                last_name: 'test_last'
+            }, function(err, user) {
+                if (err) throw err
+                else {
+                    room_user = user
+                    Room.create({
+                        name: 'TestRoom',
+                        path: 'test_path',
+                        desc: 'room for tests',
+                        owner: user
+                    }, function(err) {
+                        if (err) throw err
+                    })
+                }
             })
         })
         after(function() {
             Room.findOneAndRemove({name: 'TestRoom'}, function(err) {
                 if (err) throw err
             })
+            User.findOneAndRemove({username: 'test_user'}, function(err) {
+                if (err) throw err
+            })
         })
+
+        //Test Suites
         it('should pass 404 status code to response when invalid room name is entered', function(done) {
             var req, res, next
             req = next = {body:{}}
@@ -60,9 +81,79 @@ describe('util', function() {
             req.body.room_path = 'random'
             validateRoom(req, res, next)
             setTimeout(function(){
-                assert.isTrue(res.status.calledOnce)
+                assert.isTrue(res.status.calledWith(404))
                 done()
-            }, 500)
+            }, 100)
+        })
+        it('should return with 406 status code to response when unauthorized user tries to access room', function(done) {
+            var req, res, next
+            req = {
+                body: {room_path: 'test_path'},
+                user: {_id: 'random_id'}
+            }
+            res = {
+                status: sinon.spy(),
+                json: function(){},
+                end: function(){}
+            }
+            next = function(){}
+            validateRoom(req, res, next)
+            setTimeout(function(){
+                assert.isTrue(res.status.calledWith(406))
+                done()
+            }, 100)
+        })
+        it('should call next callback function if room validation is successful', function(done) {
+            var req, res, next
+            next = sinon.spy()
+            req = {
+                body: {room_path: 'test_path'},
+                user: room_user
+            }
+            res = {
+                status: function(){},
+                json: function(){},
+                end: function(){}
+            }
+            validateRoom(req, res, next)
+            setTimeout(function() {
+                assert.isTrue(next.calledOnce)
+                done()
+            }, 100)
+        })
+    })
+    describe('#validateBody()', function() {
+        it('should return an express middleware function', function() {
+            assert.typeOf(validateBody([]), 'function')
+        })
+        it('should send error to response if fields are not found on req.body object', function(done) {
+            var req, res, next
+            req = {
+                body: {}
+            }
+            res = {
+                status: sinon.spy(),
+                json: function() {},
+                end: function(){}
+            }
+            next = function(){}
+            validateBody(['sample_field'])(req, res, next)
+            assert.isTrue(res.status.calledWith(400))
+            done()
+        })
+        it('should invoke next callback if fields are present', function(done) {
+            var req, res, next
+            req = {
+                body: {
+                    field1: '',
+                    field2: ''
+                }
+            }
+            res = {status:function() {}, json: function() {}, end: function() {}}
+            next = sinon.spy()
+            validateBody(['field1', 'field2'])(req, res, next)
+            assert.isTrue(next.calledOnce)
+            done()
         })
     })
 })
