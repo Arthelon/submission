434dfd6b546593ddb68acb42261c8717e77b82de
Arthelon <hsing.daniel@gmail.com>
2016-07-20 16:51:44 +0800

    added database_url environment variable for build testing

--- test/app.Test.js
+++ test/app.Test.js
@@ -1,7 +1,12 @@
 var chai = require('chai')
+require("dotenv").config()
 
 var assert = chai.assert
 
 describe('app', function() {
     
+    it("should load config variables into environment", function() {
+        assert.isNotNull(process.env.DATABASE_URL)
+        assert.isNotNull(process.env.CLIENT_ID)
+    })
 })
